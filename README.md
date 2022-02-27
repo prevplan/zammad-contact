46c9c455dbcbaf9882c04f3cf14452abd83e4c14
Holger Schmermbeck <hosch@mailbox.org>
2022-02-27 21:00:02 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zammad-contact
+A quick &amp; dirty zammad contact api for handling the static contact form.
---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use DevCoder\DotEnv;
+use ZammadAPIClient\Client;
+use ZammadAPIClient\ResourceType;
+
+$absolutePathToEnvFile = __DIR__ . '/.env';
+
+(new DotEnv($absolutePathToEnvFile))->load();
+
+$ticket_id = false;
+$error_msg = false;
+
+$client = new Client([
+    'url'        => getenv('ZAMMAD_URL'),
+    'http_token' => getenv('ZAMMAD_TOKEN'),
+    // 'timeout'       => 15,                  // Sets timeout for requests, defaults to 5 seconds, 0: no timeout
+    //'debug'         => true,                // Enables debug output
+]);
+
+$ticket_data = [
+    'group_id'    => 1, // Group 'PrevHelp - Allgemein'
+    'priority_id' => 2,
+    'state_id'    => 1,
+    'title'       => htmlspecialchars($_POST['subject']),
+    'customer_id' =>  "guess:" . htmlspecialchars($_POST['email']),
+    'type'        => 'email',
+    'article'     => [
+        'type_id' => 1,
+        'from'    =>  htmlspecialchars($_POST['name']).' <'. htmlspecialchars($_POST['email']).'>',
+        'to'      => 'PrevHelp Support',
+        'subject' =>  htmlspecialchars($_POST['subject']),
+        'body'    =>  htmlspecialchars($_POST['message']),
+        'content_type' => 'text/html',
+        'type'    => 'email',
+        'sender'  => 'Customer',
+    ],
+];
+
+$ticket = $client->resource(ResourceType::TICKET);
+$ticket->setValues($ticket_data);
+$ticket->save();
+
+header('Location: https://'. $_GET['return']);
+exit;
+
+// not used at the moment...
+/*
+if ($ticket->hasError()) {
+    $error_msg = $ticket->getError();
+} else {
+    $ticket_id = $ticket->getValue('number');
+}
+*/
